21cd488544cab1a1a89d4cfbbd133b5201975159
KensukeHumburger <sasaki.cfc@outlook.jp>
2022-01-16 21:52:11 +0900

    1

--- tigers/views.py
+++ tigers/views.py
@@ -4,17 +4,23 @@ from django.views.generic.list import ListView
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+import pandas as pd
+from pandas import DataFrame
+
+# 機械学習
+import sklearn
+import pickle
+import xgboost as xgb
+from sklearn.model_selection import cross_validate, KFold
 
 def index(request):
     return render(request, 'tiger/index.html', {})
 
 def predict_input(request):
-    from tigers.include.pred_tigers import PredTigers
     return render(request, 'tiger/index.html')
 
 def predict(request):
     if request.method == 'POST':
-        from tigers.include.pred_tigers import PredTigers
         hi = request.POST.get('hi')
         low = request.POST.get('low')
         rain = request.POST.get('rain')
@@ -38,3 +44,28 @@ def predict(request):
                   {'tigers': cancelrate })         # テンプレートに渡すデータ
     else:
         return HttpResponse('predict')
+
+
+#
+class PredTigers:
+    def __init__(self):
+        self.params = {}
+    #
+    def load_model(self):
+        filename = 'tigers/files/model_XGB.pickle'
+        model = pickle.load(open(filename, 'rb'))
+        return model
+    
+    def load_data(self, params):
+        dat = {'hi':[params['hi']]
+        ,'low':[params['low']]
+        ,'rain':[params['rain']]
+        ,'snow':[params['snow']]
+        }
+        df = DataFrame(dat)
+        df = df.assign(hi = pd.to_numeric(df.hi))
+        df = df.assign(low = pd.to_numeric(df.low))
+        df = df.assign(rain = pd.to_numeric(df.rain))
+        df = df.assign(snow = pd.to_numeric(df.snow))
+        #print(df.info() )
+        return df
